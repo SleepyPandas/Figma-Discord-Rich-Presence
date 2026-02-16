3eb64a35b80c40f61584bd28608c96f688f6cae4
SleepyPandas <tommyrobotics1@gmail.com>
2026-02-16 13:08:37 -0500

    QUICKSTART: Mock Discord Rich Presence functionality by adding `rich-go` and `godotenv` dependencies and setting activity.

--- main.go
+++ main.go
@@ -1,7 +1,64 @@
-package main 
+package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/hugolgst/rich-go/client"
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Hello World")
+
+	err := godotenv.Load()
+
+	
+	if err != nil {
+		panic("Error loading .env file")
+	}
+
+	err = client.Login(os.Getenv("DISCORD_CLIENT_ID"))
+
+	if err != nil {
+		panic(err)
+	}
+
+	now := time.Now()
+	err = client.SetActivity(client.Activity{
+		State:      "Ligma",
+		Details:    "This GOOOOOOOOO GO LANGGGGGGGGGGGGGGGGGGGG :)",
+		LargeImage: "largeimageid",
+		LargeText:  "This is the large image :D",
+		SmallImage: "smallimageid",
+		SmallText:  "And this is the small image",
+		Party: &client.Party{
+			ID:         "-1",
+			Players:    12,
+			MaxPlayers: 9000,
+		},
+		Timestamps: &client.Timestamps{
+			Start: &now,
+		},
+		Buttons: []*client.Button{
+			&client.Button{
+				Label: "GitHub",
+				Url:   "https://github.com/hugolgst/rich-go",
+			},
+		},
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	// Discord will only show the presence if the app is running
+	// Sleep for a few seconds to see the update
+	fmt.Println("Sleeping...")
+	time.Sleep(time.Second * 10000)
+
+	for i := 0; i < 2; i++ {
+		main()
+	}
+
 }
